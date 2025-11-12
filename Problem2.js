63b05fa20a0389baf500575b85ad19ae200e6232
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-11-12 23:11:16 +0500

    adding the solution of Problem 2

---
+++ Problem2.js
@@ -0,0 +1,19 @@
+//**Print the number from 5 to 1 */
+
+// Method NO:1
+
+// let num = 5
+// let i = 1
+
+// for ( num ; num >= i; num--) {
+//     console.log(num);
+// }
+
+
+// Method NO:2 // **Recommended
+
+let num = 5
+
+for(let i = 1; i <= num; i++){
+    console.log(num - i + 1);
+}
