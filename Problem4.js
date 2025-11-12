fc913113bcc4f5ccb921fc68d38894edf0dc6e93
Muhammad Khan <muhammadkhan08561@gmail.com>
2025-11-12 23:11:30 +0500

    adding the solution of Problem 4

---
+++ Problem4.js
@@ -0,0 +1,21 @@
+// **Sum fo First N Natural Numbers**
+
+// Method No: 1
+let num = 5
+let sum = 0
+
+for (let i = 1; i <= num; i++) {
+    sum += i
+}
+console.log(sum);
+
+// Method No: 2 // **Recommended
+
+let num2 = 5
+
+// **FORMULA  n(n + 1) / 2
+
+let result = (num2 *  (num2 + 1)) / 2
+
+console.log(result);
+
